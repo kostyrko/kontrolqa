839719692cc5bc12b4e1b594ce3d59ddca8fd667
Mikołaj Kostyrko <mkostyrko@gmail.com>
2022-02-13 10:20:51 +0100

    add ghprefix + deploy cmd

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const metaConfig = require('./gatsby-meta-config')
 
 module.exports = {
   siteMetadata: metaConfig,
+  pathPrefix: `/kontrolqa`,
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
