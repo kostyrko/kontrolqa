1622446d239389053da4056e3d55ddeb902df101
Mikołaj Kostyrko <mkostyrko@gmail.com>
2022-03-05 14:42:29 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f985b0a44c20d632d4b9.js"
+    "url": "webpack-runtime-b52b0f08bcc1a839353a.js"
   },
   {
     "url": "styles.2d55533a8c5393bdb935.css"
@@ -38,14 +38,14 @@ self.__precacheManifest = [
     "url": "framework-727eca7a674e10b21a9f.js"
   },
   {
-    "url": "app-4372a823b8df13420983.js"
+    "url": "app-be276efafc2d380784aa.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-38fe08ef875e5952d5f0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fbc78a62e0633cf680b795edbfd24a5d"
+    "revision": "b6b34fe117769a2356bb9272aff257e1"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -53,7 +53,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "b7c595cc16f773084bc62f7606567ab2"
+    "revision": "bcd0f2d02040107b6e4b95199fad7f1c"
   },
   {
     "url": "manifest.webmanifest",
@@ -81,7 +81,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/kontrolqa/app-4372a823b8df13420983.js`))) {
+  if (!resources || !(await caches.match(`/kontrolqa/app-be276efafc2d380784aa.js`))) {
     return await fetch(event.request)
   }
 
