3c3e0bead3492d384c0504c30847b6204a3b7f9c
Mikołaj <48617990+kostyrko@users.noreply.github.com>
2023-08-31 19:25:52 +0200

    fix: cat all search + footer (#17)

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,9 +4,8 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/kostyrko">Kostyrko</a>, [Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
-      Gatsby-starter-bee (©<a href="https://github.com/JaeYeopHan">Jbee</a>)
-    </a>]
+    ©<a href="https://github.com/kostyrko">Kostyrko</a> [Built with{' '}
+    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee"></a>
+      Gatsby-starter-bee (©<a href="https://github.com/JaeYeopHan">Jbee</a>)]
   </footer>
 )
